c680abeb6c1df44c4bb330c5ff856dc644a14a42
yihuawanglv <wanglvyihua@gmail.com>
2018-02-26 00:03:44 +0800

    format

--- src/main/java/com/iyihua/framework/mvc/util/ClassUtil.java
+++ src/main/java/com/iyihua/framework/mvc/util/ClassUtil.java
@@ -22,119 +22,119 @@ import org.slf4j.LoggerFactory;
  */
 public final class ClassUtil {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ClassUtil.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClassUtil.class);
 
-    /**
-     * 获取类加载器
-     * 
-     * @return
-     */
-    public static ClassLoader getClassLoader() {
-	return Thread.currentThread().getContextClassLoader();
-    }
-
-    /**
-     * 加载类（将自动初始化）
-     */
-    public static Class<?> loadClass(String className) {
-	return loadClass(className, true);
-    }
-
-    /**
-     * 加载类
-     * 
-     * @param className
-     * @param isInitialized
-     * @return
-     */
-    public static Class<?> loadClass(String className, boolean isInitialized) {
-	Class<?> cls;
-
-	try {
-	    cls = Class.forName(className, isInitialized, getClassLoader());
-	} catch (ClassNotFoundException e) {
-	    LOGGER.error("load class failure", e);
-	    throw new RuntimeException(e);
+	/**
+	 * 获取类加载器
+	 * 
+	 * @return
+	 */
+	public static ClassLoader getClassLoader() {
+		return Thread.currentThread().getContextClassLoader();
 	}
 
-	return cls;
-    }
+	/**
+	 * 加载类（将自动初始化）
+	 */
+	public static Class<?> loadClass(String className) {
+		return loadClass(className, true);
+	}
 
-    /**
-     * 获取指定包名下的所有类
-     * 
-     * @param packageName
-     * @return
-     */
-    public static Set<Class<?>> getClassSet(String packageName) {
-	Set<Class<?>> classSet = new HashSet<Class<?>>();
-	try {
-	    Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
-	    while (urls.hasMoreElements()) {
-		URL url = urls.nextElement();
-		if (url != null) {
-		    String protocol = url.getProtocol();
-		    if (protocol.equals("file")) {
-			String packagePath = url.getPath().replace("%20", " ");
-			addClass(classSet, packagePath, packageName);
-		    } else if (protocol.equals("jar")) {
-			JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
-			if (null != jarURLConnection) {
-			    JarFile jarFile = jarURLConnection.getJarFile();
-			    if (null != jarFile) {
-				Enumeration<JarEntry> jarEntries = jarFile.entries();
-				while (jarEntries.hasMoreElements()) {
-				    JarEntry jarEntry = jarEntries.nextElement();
-				    String jarEntryName = jarEntry.getName();
-				    if (jarEntryName.endsWith(".class")) {
-					String className = jarEntryName.substring(0, jarEntryName.lastIndexOf("."))
-						.replaceAll("/", ".");
-					doAddClass(classSet, className);
-				    }
+	/**
+	 * 加载类
+	 * 
+	 * @param className
+	 * @param isInitialized
+	 * @return
+	 */
+	public static Class<?> loadClass(String className, boolean isInitialized) {
+		Class<?> cls;
+
+		try {
+			cls = Class.forName(className, isInitialized, getClassLoader());
+		} catch (ClassNotFoundException e) {
+			LOGGER.error("load class failure", e);
+			throw new RuntimeException(e);
+		}
+
+		return cls;
+	}
+
+	/**
+	 * 获取指定包名下的所有类
+	 * 
+	 * @param packageName
+	 * @return
+	 */
+	public static Set<Class<?>> getClassSet(String packageName) {
+		Set<Class<?>> classSet = new HashSet<Class<?>>();
+		try {
+			Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+			while (urls.hasMoreElements()) {
+				URL url = urls.nextElement();
+				if (url != null) {
+					String protocol = url.getProtocol();
+					if (protocol.equals("file")) {
+						String packagePath = url.getPath().replace("%20", " ");
+						addClass(classSet, packagePath, packageName);
+					} else if (protocol.equals("jar")) {
+						JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+						if (null != jarURLConnection) {
+							JarFile jarFile = jarURLConnection.getJarFile();
+							if (null != jarFile) {
+								Enumeration<JarEntry> jarEntries = jarFile.entries();
+								while (jarEntries.hasMoreElements()) {
+									JarEntry jarEntry = jarEntries.nextElement();
+									String jarEntryName = jarEntry.getName();
+									if (jarEntryName.endsWith(".class")) {
+										String className = jarEntryName.substring(0, jarEntryName.lastIndexOf("."))
+												.replaceAll("/", ".");
+										doAddClass(classSet, className);
+									}
+								}
+							}
+						}
+					}
 				}
-			    }
 			}
-		    }
+		} catch (IOException e) {
+			LOGGER.error("get class set failure", e);
+			throw new RuntimeException(e);
 		}
-	    }
-	} catch (IOException e) {
-	    LOGGER.error("get class set failure", e);
-	    throw new RuntimeException(e);
+		return classSet;
 	}
-	return classSet;
-    }
 
-    private static void doAddClass(Set<Class<?>> classSet, String className) {
-	Class<?> cls = loadClass(className, false);
-	classSet.add(cls);
-    }
-
-    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
-	File[] files = new File(packagePath).listFiles(new FileFilter() {
-	    @Override
-	    public boolean accept(File file) {
-		return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
-	    }
-	});
-	for (File file : files) {
-	    String fileName = file.getName();
-	    if (file.isFile()) {
-		String className = fileName.substring(0, fileName.lastIndexOf("."));
-		if (StringUtil.isNotEmpty(className)) {
-		    className = packageName + "." + className;
-		}
-		doAddClass(classSet, className);
-	    } else {
-		String subPackagePath = fileName;
-		if (StringUtil.isNotEmpty(packagePath)) {
-		    subPackagePath = packagePath + "/" + subPackagePath;
-		}
-		String subPackageName = fileName;
-		if (StringUtil.isNotEmpty(packageName)) {
-		    subPackageName = packageName + "." + subPackageName;
-		}
-		addClass(classSet, subPackagePath, subPackageName);
-	    }
+	private static void doAddClass(Set<Class<?>> classSet, String className) {
+		Class<?> cls = loadClass(className, false);
+		classSet.add(cls);
 	}
-    }
+
+	private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+		File[] files = new File(packagePath).listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+			}
+		});
+		for (File file : files) {
+			String fileName = file.getName();
+			if (file.isFile()) {
+				String className = fileName.substring(0, fileName.lastIndexOf("."));
+				if (StringUtil.isNotEmpty(className)) {
+					className = packageName + "." + className;
+				}
+				doAddClass(classSet, className);
+			} else {
+				String subPackagePath = fileName;
+				if (StringUtil.isNotEmpty(packagePath)) {
+					subPackagePath = packagePath + "/" + subPackagePath;
+				}
+				String subPackageName = fileName;
+				if (StringUtil.isNotEmpty(packageName)) {
+					subPackageName = packageName + "." + subPackageName;
+				}
+				addClass(classSet, subPackagePath, subPackageName);
+			}
+		}
+	}
 }
